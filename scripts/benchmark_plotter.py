c2fb22d9647ad61f4225203afdb6d952f918d019
Marco Raveri <mraveri@sissa.it>
2016-11-22 13:23:08 -0600

    Updated legacy results. Added benchmark plotter.

--- scripts/benchmark_plotter.py
+++ scripts/benchmark_plotter.py
@@ -21,163 +21,122 @@ Developed by Marco Raveri (mraveri@uchicago.edu) for the EFTCAMB code.
 
 # ***************************************************************************************
 
-__version__ = '1.0' #: version of the application
+__version__ = '0.0' #: version of the application
 
 # ***************************************************************************************
 
-""" Hard coded options """
-n_sigma = 3.0 # number of sigma in time measurements to distinguish things
+"""
+import of modules
+"""
 
-# ***************************************************************************************
-
-# initial import of modules:
+from import_benchmark import *
 
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
-import numpy as np
-import math
-import sys
-import os
-import shutil
+import matplotlib.gridspec as gridspec
 
-# define the paths:
-script_dir = os.path.dirname(os.path.abspath(__file__))
-data_dir   = script_dir+'/../Benchmark_Results'
-out_dir    = script_dir+'/../benchmark_plots'
+# ***************************************************************************************
 
-# purge results:
-shutil.rmtree(out_dir)
-os.mkdir(out_dir)
+"""
+Hard coded options
+"""
 
-# some objects to hold the benchmark results:
-class EFTCAMB_benchmark:
+x_size        = 10.0      # width of the figure, for every 10 models, in cm
+y_size        = 10.0       # height of the figure in cm
+main_fontsize = 10.0      # in points
+width         = 0.80      # width of the bars
+alpha         = 0.3       # alpha for shaded things
 
-    def __init__( self ):
-        # initialize everything to zero:
-        self.identifier   = 0
-        self.year         = 0
-        self.month        = 0
-        self.day          = 0
-        self.hour         = 0
-        self.minute       = 0
-        self.models_bench = {}
-        self.models_rel_b = {}
+# fonts:
+plt.rc('font', **{'family': 'serif', 'serif': ['Computer Modern']})
+plt.rc('text', usetex=True)
 
-    def is_close( self, other_bench ):
-        # two benchmark results are similar if they have different identifiers
-        # models_bench is within error bars.
 
-        # check whether the two benchmarks have the same models:
-        if ( sorted(self.models_bench.keys()) != sorted(other_bench.models_bench.keys()) ):
-            return False
-        # check whether all measurements are compatible:
-        for model_name in sorted( self.models_bench.keys() ):
-            value_1 = self.models_bench[model_name][0]
-            err_1   = self.models_bench[model_name][1]
-            value_2 = other_bench.models_bench[model_name][0]
-            err_2   = other_bench.models_bench[model_name][1]
-            diff_significance = abs(value_1-value_2)/math.sqrt( err_1**2 +err_2**2 )
-            if diff_significance>n_sigma:
-                return False
-        # if no significant difference is found return true:
-        return True
+# spacing:
+left    = 0.02
+right   = 1-left
+wspace  = 0.03
+bottom  = 0.25
+top     = 0.95
 
-    def compute_relative_bench( self ):
-        # get the value and error in GR:
-        value_GR = self.models_bench[ 'base_params' ][0]
-        err_GR   = self.models_bench[ 'base_params' ][1]
-        # loop over models:
-        for model in self.models_bench:
-            # get value and error of the model:
-            value_model = self.models_bench[ model ][0]
-            err_model   = self.models_bench[ model ][1]
-            # propagate errors:
-            value_rel = value_model/value_GR
-            err_rel   = value_rel*math.sqrt( (err_GR/value_GR)**2 +(err_model/value_model)**2 )
-            # save results:
-            self.models_rel_b[ model ] = [ value_rel, err_rel ]
+# colors:
+colormap = { 0: (203.0/255.0, 15.0/255.0, 40.0/255.0),
+                 1: (255.0/255.0, 165.0/255.0, 0.0),
+                 2: (42.0/255.0, 46.0/255.0, 139.0/255.0),
+                 3: (0.0/255.0, 153.0/255.0, 204.0/255.0),
+                 4: (0.0/255.0, 221.0/255.0, 52.0/255.0),
+                 5: (0.0, 0.0, 0.0),
+                 6: (0.0, 0.75, 0.75),
+               }
 
-# import the files as a nested dictionary:
-benchmark_logs_files = [ file_name for file_name in os.listdir(data_dir) if 'log' in file_name ]
-benchmark_results    = {}
-
-for file_name in benchmark_logs_files:
-    # split the file name:
-    split_name = file_name.split(".")
-    # test:
-    if split_name[0]=='benchmark' and split_name[2]=='log' and len(split_name[1])==13:
-        # initialize the dictionary:
-        results    = EFTCAMB_benchmark()
-        # get the identifier, date and time:
-        results.identifier = split_name[1]
-        results.year       = split_name[1][0:4]
-        results.month      = split_name[1][4:6]
-        results.day        = split_name[1][6:8]
-        results.hour       = split_name[1][9:11]
-        results.minute     = split_name[1][11:13]
-        # import the data:
-        with open(data_dir+'/'+file_name, 'r') as infile:
-            for line in infile:
-                split_line = line.split()
-                results.models_bench[ split_line[0] ] = [float(split_line[1]), float(split_line[3])]
-        # check the data length:
-        if ( len(results.models_bench.keys())<2 ):
-            print 'Rejecting file: ', file_name
-            print 'Number of models < 2.'
-            continue
-        # check for time drifting:
-        GR1 = results.models_bench['base_params']
-        GR2 = results.models_bench['1_GR']
-        drift_significance = abs(GR1[0]-GR2[0])/math.sqrt( GR1[1]**2 + GR2[1]**2 )
-        if ( drift_significance>n_sigma ):
-            print 'Rejecting file: ', file_name
-            print 'Significant time drifting detected'
-            continue
-        # save the result:
-        benchmark_results[ results.identifier ] = results
-
-# do other computations:
-# sort the model keys:
-sorted_benchmark_results_keys = sorted(benchmark_results)
-
-# prune the benchmark results removing very similar results:
-for ind, benchmark_name in enumerate(sorted_benchmark_results_keys):
-    if ind==0:
-        continue
-    prev_key = sorted_benchmark_results_keys[ind-1]
-    if benchmark_results[benchmark_name].is_close( benchmark_results[prev_key] ):
-        del benchmark_results[prev_key]
-
-# update sorted list:
-sorted_benchmark_results_keys = sorted(benchmark_results)
-
-# do derived calculations:
-for res in sorted_benchmark_results_keys:
-    benchmark_results[res].compute_relative_bench()
+# ***************************************************************************************
 
 # plot the single benchmarks:
 for res in sorted_benchmark_results_keys:
     # process:
     labels = sorted(benchmark_results[res].models_rel_b.keys())
     x     = np.arange( len(benchmark_results[res].models_rel_b) )
-    y     = [ temp[0] for temp in [ benchmark_results[res].models_rel_b[key] for key in labels ] ]
-    y_err = [ temp[1] for temp in [ benchmark_results[res].models_rel_b[key] for key in labels ] ]
+    y     = np.array([ temp[0] for temp in [ benchmark_results[res].models_rel_b[key] for key in labels ] ])
+    y_err = np.array([ temp[1] for temp in [ benchmark_results[res].models_rel_b[key] for key in labels ] ])
+    num   = len(x)
+    eff_x_size = float(num)/10.0*x_size
     # plot:
     fig, ax = plt.subplots()
-    ax.bar( x, y, yerr=y_err )
-
-
-
-
-
+    gs = gridspec.GridSpec(1, 1)
+    ax = plt.subplot(gs[0, 0])
+    fig.set_size_inches( eff_x_size/2.54, y_size/2.54 )
+    # plot the data:
+    rect_1 = ax.bar( x+0.1, y     , width, yerr=y_err, ecolor='k', color=colormap[3], linewidth=0.8 )
+    # plot the upper shaded error bar:
+    rect_2 = ax.bar( x+0.1, +y_err, width, bottom=y  , color='k' , alpha=alpha      , linewidth=0   )
+    # plot the lower shaded error bar:
+    rect_3 = ax.bar( x+0.1, -y_err, width, bottom=y  , color='k' , alpha=alpha      , linewidth=0   )
+    # plot the grid:
+    ax.grid(True)
+    # set the x lim:
+    ax.set_xlim( [ np.amin(x), np.amax(x)+1.0 ] )
+    # get human readable names:
+    labels = [ label.replace( '_', ' ' ) for label in labels ]
+    # set x labels:
+    ax.set_xticks( np.array( xrange( len(labels) ) ) +0.5 )
+    ax.set_xticklabels( labels, rotation=90, horizontalalignment='center', fontsize=0.8*main_fontsize )
+    # y-axis positioning and size of the tick labels:
+    fig.canvas.draw()
+    ax.tick_params(axis='y', which='both', labelleft='on', labelright='on',labelsize=0.9*main_fontsize)
+    ax.set_yticklabels( [item.get_text() for item in ax.get_yticklabels()], fontsize=0.9*main_fontsize )
+    ax.get_yticklabels()[0].set_verticalalignment('bottom')
+    ax.get_yticklabels()[ len(ax.get_yticklabels())/2 ].set_verticalalignment('bottom')
+    ax.get_yticklabels()[ len(ax.get_yticklabels())/2-1 ].set_verticalalignment('top')
+    ax.get_yticklabels()[-1].set_verticalalignment('top')
+    # y-axis label:
+    ax.set_ylabel('EFTCAMB time / CAMB time', fontsize=main_fontsize)
+    ax.yaxis.set_label_position("left")
+    # get the size of the labels:
+    fig.canvas.draw()
+    # get the default renderer:
+    renderer = matplotlib.backend_bases.RendererBase()
+    # default dpi is 72.0:
+    default_dpi = 72.0
+    # get the size of the figure:
+    figure_x_size = fig.get_size_inches()[0] #: in inches
+    figure_y_size = fig.get_size_inches()[1] #: in inches
+    # get the maximum label size:
+    max_x_tick_label = [0.0,0.0]
+    for xlabel in ax.get_xticklabels():
+        x_dimension = xlabel.get_window_extent(renderer).width/default_dpi   #: in inches
+        y_dimension = xlabel.get_window_extent(renderer).height/default_dpi  #: in inches
+        if x_dimension>max_x_tick_label[0]: max_x_tick_label[0]=x_dimension
+        if y_dimension>max_x_tick_label[1]: max_x_tick_label[1]=y_dimension
+    bottom_size = max_x_tick_label[1]+ 2.0*matplotlib.rcParams['axes.labelpad']/default_dpi
+    # appearence:
+    gs.update( bottom= bottom_size/figure_y_size, top=top, left=left, right=right, wspace=wspace)
+    # plot title:
+    title = 'EFTCAMB Benchmark results at '+benchmark_results[res].hour+':'+benchmark_results[res].minute+' of '+benchmark_results[res].day+'/'+benchmark_results[res].month+'/'+benchmark_results[res].year
+    ax.set_title( title, fontsize=main_fontsize, loc='center')
     # save and close figure:
     plt.savefig(out_dir+'/'+str(res)+'.pdf')
     plt.savefig(out_dir+'/'+str(res)+'.png')
     plt.clf()
-
-
-
-
 
 exit(0)
